d3a76c73a60d2af494444f1a72c66a0527c2389b
MWShay <mingweihsieh@outlook.com>
2025-05-16 22:36:28 +0800

    新增 API no26~31 功能

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,11 +1,169 @@
-// const { dataSource } = require('../db/data-source')
-// const appError = require('../utils/appError')
-// const logger = require('../utils/logger')('creditPackageController')
-// const {isUndefined, isNotValidString, isNotValidInteger} = require('../utils/validUtils')
-// const bcrypt = require('bcrypt')
-// const isAuth = require('../middleware/isAuth')
+const config = require('../config/index')
+const logger = require('../utils/logger')('Admin')
+const appError = require('../utils/appError')
 
-// const adminController = {
+const { dataSource } = require('../db/data-source')
+const {isUndefined, isNotValidString, isNotValidInteger} = require('../utils/validUtils')
+const bcrypt = require('bcrypt')
+const isAuth = require('../middleware/isAuth')
+
+const { dataSource } = require('../db/data-source')
+const config = require('../config/index')
+const appError = require('../utils/appError')
+const { dataSource } = require('../db/data-source')
+const { getOrdersData,getSingleOrderData } = require('../services/orderService')
+const { proposeEventValid,isUndefined,isNotValidString,isNotValidUuid } = require('../utils/validUtils');
+
+const ERROR_STATUS_CODE = 400;
+
+const adminController = {
+    //取得所有一般會員資料 no.26
+    async getAllUsers(req, res) {
+
+        const userRepository = dataSource.getRepository('User')
+        const users = await userRepository.find({
+            select: ['serialNo', 'name', 'role', 'count'],
+            where: { role: 'USER' }
+        });
+
+        res.status(200).json({
+            status: 'success',
+            message: '取得成功',
+            data: {
+                users
+            }
+        });
+    },
+    //取得所有活動列表 no.29
+    async getAllEvents(req, res) {
+
+        const eventRepository = dataSource.getRepository('Event')
+        const events = await eventRepository.find({
+            select: ['id', 
+                'title', 
+                'cover_image_url', 
+                'location', 
+                'start_at',
+                'status'], // 要添加 sale_status, sale_rate
+        });
+
+        res.status(200).json({
+            status: true,
+            message: '取得活動列表成功',
+            data: {
+                events
+            }
+        });
+    },
+    //取得單一活動資訊 no.27
+    async getSingleEvent(req, res) {
+        const { eventId } = req.params
+        if (isUndefined(eventId) || isNotValidString(eventId) || isNotValidUuid(eventId)) {
+            next(appError(ERROR_STATUS_CODE, '欄位未填寫正確'))
+            return
+        }
+        const eventRepository = dataSource.getRepository('Event')
+        const event = await eventRepository.findOne({
+            select: ['id', 'title', 'cover_image_url', 'section_image_url', 'created_at', 'updated_at', 'status'],
+            where: { id: eventId }
+        })
+
+        res.status(200).json({
+            status: true,
+            message: "取得活動成功",
+            data: event
+        })
+    },
+    //新增活動審核 no.28
+    async patchEvent(req, res) {
+        const { eventId } = req.params
+        if (isUndefined(eventId) || isNotValidString(eventId) || isNotValidUuid(eventId)) {
+            next(appError(ERROR_STATUS_CODE, '欄位未填寫正確'))
+            return
+        }
+        const eventRepository = dataSource.getRepository('Event')
+        const event = await eventRepository.findOne({
+            select: ['id', 'title', 'cover_image_url', 'section_image_url', 'created_at', 'updated_at', 'status'],
+            where: { id: eventId }
+        })
+
+        res.status(200).json({
+            status: true,
+            message: "審核活動成功",
+            data: event
+        })
+    },
+
+    //取得單一會員資料 no.30
+    async getSingleUser(req, res, next) {
+        const { userId } = req.params
+        if (isUndefined(eventId) || isNotValidString(eventId) || isNotValidUuid(eventId)) {
+            next(appError(ERROR_STATUS_CODE, '欄位未填寫正確'))
+            return
+        }
+        const singleUser = dataSource.getRepository('User')
+        const user = await singleUser.findOne({
+            select: ['id', 'name', 'email', 'role', 'created_at'],
+            where: { id: userId }
+        })
+        if (!user) {
+            logger.warn('使用者不存在')
+            return next(appError(400, '使用者不存在'))
+        }
+    
+        res.status(200).json({
+            status: true,
+            message: "取得會員資料成功成功",
+            data: order
+        })
+    },
+
+    // 使用者審核 no.31
+    async patchUser(req, res, next) {
+        const { userId } = req.params
+        const { role } = req.body
+        if (isUndefined(userId) || isNotValidString(userId) || isNotValidUuid(userId)) {
+            next(appError(ERROR_STATUS_CODE, '欄位未填寫正確'))
+            return
+        }
+        if (role !== 'COACH' && role !== 'USER') {
+            next(appError(ERROR_STATUS_CODE, '欄位未填寫正確'))
+            return
+        }
+        const userRepository = dataSource.getRepository('User')
+        const existingUser = await userRepository.findOne({
+            select: ['id', 'name', 'email', 'role'],
+            where: { id: userId }
+        })
+
+        if (!existingUser) {
+            logger.warn('使用者不存在')
+            return next(appError(400, '使用者不存在'))
+        }
+
+        const updatedUser = await userRepository.patch({
+            id: userId,
+            role: 'USER'
+        }, {
+            role
+        })
+        if (updatedUser.affected === 0) {
+            logger.warn('更新使用者失敗')
+            return next(appError(400, '更新使用者失敗'))
+        }
+        const savedUser = await userRepository.findOne({
+            select: ['name', 'role'],
+            where: { id: userId }
+        })
+        res.status(200).json({
+            status: true,
+            message: "編輯成功",
+            data: savedUser
+        })
+    }
+}
+module.exports = adminController
+
 //     async postAdminCourse(req, res, next) {
 //         const {
 //             user_id: userId, skill_id: skillId, name, description, start_at: startAt, end_at: endAt,
@@ -195,4 +353,3 @@
 //     }
 // }
 
-// module.exports = adminController
--- routes/admin.js
+++ routes/admin.js
@@ -1,20 +1,33 @@
-// const express = require('express')
+const express = require('express')
 
-// const router = express.Router()
-// const { dataSource } = require('../db/data-source')
-// const logger = require('../utils/logger')('Admin')
-// const  appError = require('../utils/appError')
-// const isAuth = require('../middleware/isAuth')
+
+const router = express.Router()
+const { dataSource } = require('../db/data-source')
+const logger = require('../utils/logger')('Admin')
+const  appError = require('../utils/appError')
+const isAuth = require('../middleware/isAuth')
 // const isCoach = require('../middleware/isCoach')
-// const {isUndefined, isNotValidString, isNotValidInteger} = require('../utils/validUtils')
-// const handleErrorAsync = require('../utils/handleErrorAsync')
-// const adminController = require('../controllers/admin')
+const {isUndefined, isNotValidString, isNotValidInteger} = require('../utils/validUtils')
+const handleErrorAsync = require('../utils/handleErrorAsync')
+const adminController = require('../controllers/admin')
 
 
-// router.post('/coaches/courses', isAuth, isCoach, handleErrorAsync(adminController.postAdminCourse))
+// 26.取得所有一般會員資料
+router.get('/admin/users', isAuth, handleErrorAsync(adminController.getAllUsers))
 
-// router.put('/coaches/courses/:courseId', isAuth, isCoach, handleErrorAsync(adminController.putAdminCourse))
+// 27.取得單一活動資訊
+router.get('/admin/events/:eventId', isAuth, handleErrorAsync(adminController.getSingleEvent))
 
-// router.post('/coaches/:userId', handleErrorAsync(adminController.postAdminCoach))
+// 28.新增活動審核
+router.patch('/admin/events/:eventId', isAuth, handleErrorAsync(adminController.patchEvent))
 
-// module.exports = router
+// 29.取得所有活動列表
+router.get('/admin/events', isAuth, handleErrorAsync(adminController.getAllEvents))
+
+// 30.查看單一會員詳細資訊
+router.get('/admin/users/:userId', isAuth, handleErrorAsync(adminController.getSingleUser))
+
+// 31.使用者審核
+router.patch('/admin/users/:userId', isAuth, handleErrorAsync(adminController.patchUser))
+
+module.exports = router
